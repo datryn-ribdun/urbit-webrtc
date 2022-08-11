cdb1248143698e6af049b592d27c47510c2dfdda
datryn-ribdun <104508587+datryn-ribdun@users.noreply.github.com>
2022-08-10 21:50:29 -0400

    minor cleanup

--- campfire/ui/src/components/Servers.tsx
+++ campfire/ui/src/components/Servers.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import useUrchatStore from '../useUrchatStore';
+import { useStore } from '../stores/root';
 
 // eslint-disable-next-line
-function IceServers() {
-  const servers = useUrchatStore(state => state.configuration.iceServers);
+export const IceServers = () => {
+  const { urchatStore } = useStore();
+  const servers = urchatStore.configuration.iceServers;
 
   return (
     <div className="iceServers">
       <h4>Ice servers</h4>
-      { servers.map((server, idx) => (
+      {servers.map((server, idx) => (
         <pre key={idx}>{server}</pre>
       ))}
     </div>
